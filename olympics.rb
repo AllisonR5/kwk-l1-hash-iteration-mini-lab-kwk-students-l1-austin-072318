45e1821e916987632d3704cfcb156f824dabb863
AllisonR5 <allicraft05@gmail.com>
2018-07-25 16:47:21 +0000

    automatically backed up by learn

--- olympics.rb
+++ olympics.rb
@@ -1,10 +1,12 @@
 summer_olympics = {:Sydney => "2000", :Athens => "2004",
 :Beijing => "2008", :London => "2012"}
-summer_olympics.replace!(&:upcase)
 summer_olympics.merge!(:Atlanta => "1996")
  summer_olympics.each do |place, year|
  puts "The #{year} summer olympics took place in #{place}."
 end 
 
+upcased_city =[summer_olympics] << upcase
+summer_olympics.each do |place|
+  
 
 
